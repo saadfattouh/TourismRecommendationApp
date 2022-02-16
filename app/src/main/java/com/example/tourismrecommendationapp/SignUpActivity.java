e1e67d9385ded49b72587f0f0337d3af691739a7
saadfattouh <saadfattoh22@gmail.com>
2022-02-16 18:58:40 +0200

    initial_2

---
+++ app/src/main/java/com/example/tourismrecommendationapp/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.example.tourismrecommendationapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SignUpActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
+
+
+}
--- app/src/main/java/com/example/tourismrecommendationapp/model/place.java
+++ app/src/main/java/com/example/tourismrecommendationapp/model/place.java
@@ -4,13 +4,51 @@ public class place {
     private int id;
     private int tagName;
     private String name;
-    private String numOfVisits;
+    private String region;
+    private String image;
+    private double lat;
+    private double lon;
 
-    public place(int id, int tagName, String name, String numOfVisits) {
+    public place(int id, int tagName, String name, String region, String image, double lat, double lon) {
         this.id = id;
         this.tagName = tagName;
         this.name = name;
-        this.numOfVisits = numOfVisits;
+        this.region = region;
+        this.image = image;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
     }
 
     public int getId() {
@@ -35,13 +73,5 @@ public class place {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getNumOfVisits() {
-        return numOfVisits;
-    }
-
-    public void setNumOfVisits(String numOfVisits) {
-        this.numOfVisits = numOfVisits;
     }
 }
